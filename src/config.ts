ae0ea8dd3c99b9fb3a84f7453faa5c753e3cc3d8
javierCh77 <javierchavarria21@gmail.com>
2023-11-16 09:44:38 -0300

    update config back

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,9 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyAlLyv920wY2vteX61Xw6COAR89Zr8kcd4",
-    authDomain: "todo-app-7daba.firebaseapp.com",
-    databaseURL: "https://todo-app-7daba-default-rtdb.firebaseio.com",
-    projectId: "todo-app-7daba",
-    storageBucket: "todo-app-7daba.appspot.com",
-    messagingSenderId: "569289826045",
-    appId: "1:569289826045:web:e47c7235c473d1e91b6894",
-    measurementId: "G-2NCFY13CHP"
+  apiKey: "AIzaSyAlPksXp2BSz04_FufjiUSwmuOV1wV0Oo4",
+  authDomain: "cmep-e84d9.firebaseapp.com",
+  projectId: "cmep-e84d9",
+  storageBucket: "cmep-e84d9.appspot.com",
+  messagingSenderId: "964781996538",
+  appId: "1:964781996538:web:119a006645436bcfbbcc43"
   };
+  
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,11 +2,14 @@
   export const environment = {
     production: false,
     firebaseConfig: {
-      apiKey: "AIzaSyBviayQ5f6uZbxf1W6yzT3fUrxPTJe9lBk",
-      authDomain: "login-e05df.firebaseapp.com",
-      projectId: "login-e05df",
-      storageBucket: "login-e05df.appspot.com",
-      messagingSenderId: "57103343767",
-      appId: "1:57103343767:web:c328202354fc73b1c689d2"
+      apiKey: "AIzaSyAlPksXp2BSz04_FufjiUSwmuOV1wV0Oo4",
+  authDomain: "cmep-e84d9.firebaseapp.com",
+  projectId: "cmep-e84d9",
+  storageBucket: "cmep-e84d9.appspot.com",
+  messagingSenderId: "964781996538",
+  appId: "1:964781996538:web:119a006645436bcfbbcc43"
     }
   };
+  
+  
+  
